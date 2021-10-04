8d91971351b313a7831c99e5f84fe795bb03befa
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-04 12:38:04 -0500

    Analizando la composición

--- src/app/App.js
+++ src/app/App.js
@@ -1,50 +1,18 @@
 import React from "react";
 
 // importamos nuestro provider, (el estado de nuestra web app)
-// import { TareaProvider } from "../tareaContex";
+import { TareaProvider } from "../tareaContex";
 
-// import { AppUI } from "./AppUI";
+import { AppUI } from "./AppUI";
 
 import "../css/App.css";
 
 function App() {
-  const [state, setState] = React.useState("Estado compartido");
-
   return (
-    <React.Fragment>
-      <TareaHeader>
-        <TareaCounter />
-        <TareaSearch />
-      </TareaHeader>
-
-      <TareaList>
-        <TareaItem state={state} />
-      </TareaList>
-    </React.Fragment>
+    <TareaProvider>
+      <AppUI />
+    </TareaProvider>
   );
 }
-function TareaHeader({ children }) {
-  return <header>{children}</header>;
-}
-function TareaList({ children }) {
-  return <section className="TareList-container">{children}</section>;
-}
-function TareaCounter() {
-  return <p>TareaCounter</p>;
-}
-function TareaSearch() {
-  return <p>TareaSearch</p>;
-}
-function TareaItem({ state }) {
-  return <p>TareaItems: {state}</p>;
-}
-
-// function App() {
-//   return (
-//     <TareaProvider>
-//       <AppUI />
-//     </TareaProvider>
-//   );
-// }
 
 export default App;
