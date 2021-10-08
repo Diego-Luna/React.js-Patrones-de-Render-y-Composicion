1eebe2b6a69498ecaf90f3049fffeb288dcbb3a4
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-08 10:55:06 -0500

    estilos de alerta

--- src/components/ChangeAlert/index.js
+++ src/components/ChangeAlert/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
 
+import "../../css/components/ChangeAlert.css";
+
 function Changealert({ show, toggleShow }) {
   if (show) {
     return (
-      <div>
-        <p>Hubo cambio</p>
+      <div className="alert-container">
+        <p>Hubo cambio en las tareas</p>
         <button onClick={() => toggleShow(false)} type="button">
           Volver a cargar la información
         </button>
       </div>
     );
   } else {
-    return <p>No hubo cambio</p>;
+    return null;
   }
 }
 
